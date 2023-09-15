8ecd4822660e7dc28abf58220855e5d9fa0c3087
razrhj <razrhj@outlook.com>
2023-09-15 10:19:17 +0800

    use cmake to build first

---
+++ JsonParser/src/main.cpp
@@ -0,0 +1,31 @@
+#include "../include/DataTypes.hpp"
+#include "../include/json_parser.hpp"
+#include <cstdio>
+#include <regex>
+#include <unordered_map>
+
+int main(int argc, char *argv[]) {
+
+  JsonParser jp;
+
+  jp.readFromFile("../json_examples/json_example2.json");
+  jp.scanBuffer();
+  if (jp.parse() != nullptr) {
+    printf("true\n");
+  } else {
+    printf("false\n");
+  }
+
+  // jp.printValues();
+
+  std::string str = R"("zone")";
+
+  std::unordered_map<std::string, DataTypes::Value *> hash = jp[str];
+
+  printf("%s: \n", str.c_str());
+  for (auto it = hash.begin(); it != hash.end(); it++) {
+    printf("\t%s\n", it->first.c_str());
+  }
+
+  return 0;
+}
